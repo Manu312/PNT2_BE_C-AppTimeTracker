3798f4d981708686509deabf747c5c4dfe199f70
Manu312 <manumercado14@gmail.com>
2024-05-20 16:25:32 -0300

    Mejorar placeholder 'LoginScreen'

--- my-app/navigation/AppNavigation.js
+++ my-app/navigation/AppNavigation.js
@@ -7,10 +7,10 @@ const Stack = createStackNavigator();
 export default function AppNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen 
-        name="Login" 
-        component={LoginScreen} 
-        options={{ headerShown: false }} 
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
